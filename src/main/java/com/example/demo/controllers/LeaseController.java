f2c9fd6ca93019dfd3caa1124dc40744cbc55d99
tanmay-a-sharma <40327232+tanmay-a-sharma@users.noreply.github.com>
2024-06-22 00:13:01 -0400

    new function

--- src/main/java/com/example/demo/controllers/LeaseController.java
+++ src/main/java/com/example/demo/controllers/LeaseController.java
@@ -1,21 +1,27 @@
 package com.example.demo.controllers;
 
 import com.example.demo.models.LeaseModel;
-import com.example.demo.services.PostLease;
+import com.example.demo.services.Leasing;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class ListingController {
-    private final PostLease postLease;
+public class LeaseController {
+    private final Leasing postLease;
 
-    public ListingController(PostLease postLease) {
+    public LeaseController(Leasing postLease) {
         this.postLease = postLease;
     }
 
-    @PostMapping("/listings")
+    @PostMapping("/lease")
     public void postListing(@RequestBody LeaseModel lease) {
         postLease.postLease(lease);
+    }
+
+    @GetMapping("/lease")
+    public void getLease(@RequestBody LeaseModel lease) {
+        postLease.getLease(lease);
     }
 }
--- src/main/java/com/example/demo/services/Leasing.java
+++ src/main/java/com/example/demo/services/Leasing.java
@@ -4,10 +4,16 @@ import com.example.demo.models.LeaseModel;
 import org.springframework.stereotype.Service;
 
 @Service
-public class PostLease {
+public class Leasing {
 
     public void postLease(LeaseModel lease) {
         // functionality to post to database
         System.out.println("Lease posted");
+    }
+
+    public LeaseModel getLease(LeaseModel lease) {
+        // functionality to get from database
+        System.out.println("Lease retrieved");
+        return null;
     }
 }
